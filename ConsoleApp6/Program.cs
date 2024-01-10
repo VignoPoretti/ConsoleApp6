2779cee969c15141f49dc52e1b355a3e79f17e01
Utente <Utente@LAPTOP-M4JBAMO1>
2024-01-10 16:34:10 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector (2,2);
+            Vector v2 = new Vector(3, 3);
+
+            Vector v3 = Vector.Parse("4,4");
+
+            Console.WriteLine("({0}, {1}), ({2}, {3}),  somma ({4}, {5})", v1.X, v1.Y, v2.X, v2.Y, v1.somma(v2).X, v1.somma(v2).Y);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp6/Vector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Vector
+    {
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public Vector(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Vector somma(Vector v)
+        {
+            float x= v.X+this.X;
+            float y= v.Y+this.Y;
+
+            return( new Vector(x,y));
+        }
+        public static Vector Parse(string s)
+        {
+            string[] Coord= s.Split(',');
+            float x= float.Parse(Coord[0]);
+            float y= float.Parse(Coord[1]);   
+            return (new Vector(x,y));
+        }
+    }
+}
